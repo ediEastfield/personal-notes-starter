d79b46b12547871edba9205523060774b62da6a0
5751mn <edi.eastfield99@gmail.com>
2022-08-19 03:36:03 +0800

    input note

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NoteList from "./NoteList";
 import { getInitialData } from '../utils/index';
+import NoteInput from "./NoteInput";
 
 class NoteApp extends React.Component {
     constructor(props) {
@@ -10,6 +11,7 @@ class NoteApp extends React.Component {
         }
 
         this.onDeleteHandler = this.onDeleteHandler.bind(this);
+        this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
     }
 
     onDeleteHandler(id) {
@@ -17,15 +19,40 @@ class NoteApp extends React.Component {
         this.setState({ notes });
     }
 
+    onAddNoteHandler({ title, body }) {
+        this.setState((prevState) => {
+            return {
+                notes: [
+                    ...prevState.notes,
+                    {
+                        id: +new Date(),
+                        title,
+                        body,
+                        createdAt: new Date().toISOString(),
+                        archived: false,
+                    }
+                ]
+            }
+        });
+    }
+
     render() {
         return (
-            <div className="note-app__body">
-                <h2>Catatan Aktif</h2>
-                <NoteList 
-                notes={this.state.notes} 
-                onDelete={this.onDeleteHandler}
-                />
-            </div>
+            <body>
+                <div className="note-app__header">
+                    <h1>Notes</h1>
+                </div>
+                <div className="note-app__body">
+                    <div></div>
+                    <h2>Buat Catatan</h2>
+                    <NoteInput addNote={this.onAddNoteHandler} />
+                    <h2>Catatan Aktif</h2>
+                    <NoteList 
+                    notes={this.state.notes} 
+                    onDelete={this.onDeleteHandler}
+                    />
+                </div>
+            </body>
         );
     }
 }
